94ba36dbcc74ddbf91323a5cc16bc641d4b1c234
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-01 21:43:46 +0200

    my itoa

--- src/tinyprintf.c
+++ src/tinyprintf.c
@@ -1,41 +1,52 @@
 #include "tinyprintf.h"
 #include <stdio.h>
 
-int decimal(int val)
+int my_strlen(const char *str)
 {
-    int count = 0;
-    int reversed = 0;
-    int remainder = 0;
-    char to_print;
-    
-    if (val == 0)
+    int len = 0;
+    while (str[len] != '\0')
     {
-        putchar('0');
-        return 1;
+        len++;
     }
-    if (val < 0)
-    {
-        putchar('-');
-        val = -val;
-        count++;
-    }
-    
-    while (val != 0)
-    {
-        remainder = val % 10;
-        reversed = reversed * 10 + remainder;
-        val/= 10;
-    }
+    return len;
+}
 
-    while (reversed != 0)
+char convertToDec(int n)
+{
+    if (n >= 0 && n <= 9)
     {
-        remainder = reversed % 10;
-        to_print = remainder + '0';
-        putchar(to_print);
-        reversed/= 10;
-        count++;
+        return (n + '0');
     }
-    return count;
+    return (n + 'a' - 10);
+}
+
+void reverseString(char *s)
+{
+    int len = my_strlen(s);
+
+    for (int i = 0; i < len / 2; i++)
+    {
+        char temp = s[i];
+        s[i] = s[len - i - 1];
+        s[len - i - 1] = temp;
+    }
+}
+
+int my_itoa_base(int n, int base)
+{
+    char s[512];
+    int i = 0;
+
+    while (n > 0)
+    {
+        s[i++] = convertToDec(n % base);
+        n/= base;
+    }
+    s[i] = '\0';
+
+    reverseString(s);
+    fputs(s, stdout);
+    return my_strlen(s);
 }
 
 int tinyprintf(const char *format, ...)
@@ -47,7 +58,7 @@ int tinyprintf(const char *format, ...)
 
     va_list ap;
     va_start(ap, format);
-    
+
     int s;
     int count = 0;
     int i = 0;
@@ -58,20 +69,29 @@ int tinyprintf(const char *format, ...)
         {
             switch (format[i + 1])
             {
+            case 'x':
+                s = va_arg(ap, int);
+                count+= my_itoa_base(s, 16);
+                i++;
+                break;
+            case 'o':
+                s = va_arg(ap, int);
+                count+= my_itoa_base(s, 8);
+                i++;
+                break;
             case 'c':
                 s = va_arg(ap, int);
-                putchar(s);
-                count++;
+                fputc((s + '0'), stdout);
                 i+=1;
                 break;
             case 'u':
-                s = va_arg(ap, unsigned);
-                count+= decimal(s);
+                s = va_arg(ap, int);
+                count+= my_itoa_base(s, 10);
                 i++;
                 break;
             case 'd':
                 s = va_arg(ap, int);
-                count+= decimal(s);
+                count+= my_itoa_base(s, 10);
                 i++;
                 break;
             default:
