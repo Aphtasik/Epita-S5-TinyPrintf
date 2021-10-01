6587374a6422d8f47a5ccb2eb49c0ef1df38c464
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-01 23:10:07 +0200

    unsigned handle

--- src/tinyprintf.c
+++ src/tinyprintf.c
@@ -54,18 +54,35 @@ int my_itoa_base(int n, int base)
     return my_strlen(s);
 }
 
-int itaoi_handle_signed(int isSigned, int n, int base)
+int my_itoa_base_unsigned(unsigned int n, int base)
 {
-    if (isSigned == 1 && n < 0)
+    if (n == 0)
+    {
+        putchar('0');
+        return 1;
+    }
+    char s[512];
+    int i = 0;
+
+    while (n > 0)
+    {
+        s[i++] = convertToDec(n % base);
+        n/= base;
+    }
+    s[i] = '\0';
+
+    reverseString(s);
+    fputs(s, stdout);
+    return my_strlen(s);
+}
+
+int itoa_handle_signed(int n, int base)
+{
+    if ( n < 0)
     {
         n = -n;
         putchar('-');
         return my_itoa_base(n, base) + 1;
-    }
-    else if (isSigned == 0 && n < 0)
-    {
-        n = -n;
-        return my_itoa_base(n, base);
     }
     return my_itoa_base(n, base);
 }
@@ -107,20 +124,20 @@ int tinyprintf(const char *format, ...)
                 count += print_string(va_arg(ap, char*));
                 break;
             case 'x':
-                count+= itaoi_handle_signed(0, va_arg(ap, int), 16);
+                count+= my_itoa_base_unsigned(va_arg(ap, int), 16);
                 break;
             case 'o':
-                count+= itaoi_handle_signed(0, va_arg(ap, int), 8);
+                count+= my_itoa_base_unsigned(va_arg(ap, int), 8);
                 break;
             case 'c':
                 putchar(va_arg(ap, int));
                 count++;
                 break;
             case 'u':
-                count+= itaoi_handle_signed(0, va_arg(ap, int), 10);
+                count+= my_itoa_base_unsigned(va_arg(ap, int), 10);
                 break;
             case 'd':
-                count+= itaoi_handle_signed(1, va_arg(ap, int), 10);
+                count+= itoa_handle_signed(va_arg(ap, int), 10);
                 break;
             default:
                 putchar(format[i + 1]);
